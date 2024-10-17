9d8be89ba5cb6f4e4a8692aa778730d771f9aa19
anjumnr99 <anjumanowara99@gmail.com>
2024-10-18 03:04:53 +0600

    updated

--- src/Authentication/AuthProvider.jsx
+++ src/Authentication/AuthProvider.jsx
@@ -9,69 +9,68 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState({});
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
-        // Google login
-        const googleLogin = () => {
-            setLoading(true);
-            return signInWithPopup(auth, googleProvider);
-        };
-    
-        // email password sign up
-        const signUpWithEmailAndPassword = (email, password) => {
-            setLoading(true);
-            return createUserWithEmailAndPassword(auth, email, password);
-        };
-    
-        // email password login
-        const loginWithEmailAndPassword = (email, password) => {
-            setLoading(true);
-            return signInWithEmailAndPassword(auth, email, password);
-        };
+    // Google login
+    const googleLogin = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    };
 
-                // Log out
-                const logOut = () => {
-                    setLoading(true);
-                    return signOut(auth);
-                };
-        
-        
-                useEffect(()=>{
-                    const unSubscriber = onAuthStateChanged(auth,(user)=>{
-                        setUser(user);
-                        setLoading(false);
-                    });
-            
-                    return()=>{
-                        unSubscriber()
-                    }
-            
-                },[]);
-        
-                // User updating
-                const userUpdate = (name, image) => {
-                    return (
-                        updateProfile(auth.currentUser, {
-                            displayName: name, photoURL: image
-                        })
-                    )
-                }
+    // email password sign up
+    const signUpWithEmailAndPassword = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    // email password login
+    const loginWithEmailAndPassword = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    // Log out
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    };
 
 
-                const authInfo = {
-                    googleLogin,
-                    signUpWithEmailAndPassword,
-                    loginWithEmailAndPassword,
-                    user,
-                    logOut,
-                    userUpdate,
-                    loading
-                }
-                return (
-                    <AuthContext.Provider value={authInfo}>
-                        {children}
-                    </AuthContext.Provider>
-                );
-        
-    
+    useEffect(() => {
+        const unSubscriber = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+            setLoading(false);
+        });
+
+        return () => {
+            unSubscriber()
+        }
+
+    }, []);
+
+    // User updating
+    const userUpdate = (name, image) => {
+        return (
+            updateProfile(auth.currentUser, {
+                displayName: name, photoURL: image
+            })
+        )
+    }
+
+    const authInfo = {
+        googleLogin,
+        signUpWithEmailAndPassword,
+        loginWithEmailAndPassword,
+        user,
+        logOut,
+        userUpdate,
+        loading
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+
 };
 
 export default AuthProvider;
